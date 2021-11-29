14257dfb08122622c93e55092ce83127aafc76d3
Yaroslav1690 <yara1690@mail.ru>
2021-11-29 20:18:56 +0300

    Update дз5.py

--- дз5.py
+++ дз5.py
@@ -19,4 +19,4 @@ a = int(input("Введите искомый элемент: "))
 print(poisk(d, a))
 assert poisk([], 6) == None
 assert poisk([3, 7, 1, 8, 5, 9, 2, 4, 6], 0) == None
-assert poisk([3, 7, 1, 3, 1, 8, 7, 8, 5, 9, 4, 6, 2, 4, 6], 8) == 7
+assert poisk([3, 7, 1, 3, 1, 8, 7, 8, 5, 9, 4, 6, 2, 4, 6], 8) == 5
