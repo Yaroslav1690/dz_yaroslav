86bfba7348f5c2b7b40e527e37ce78fe4a3f6700
Yaroslav1690 <yara1690@mail.ru>
2021-11-30 08:59:42 +0300

    Update дз5.py

--- дз5.py
+++ дз5.py
@@ -1,6 +1,5 @@
-d = [3, 7, 1, 8, 5, 9, 2, 4, 6]
+d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 def poisk(d, a):
-    d = sorted(d)
     low = 0
     high = len(d) - 1
     while low <= high:
@@ -18,5 +17,5 @@ def poisk(d, a):
 a = int(input("Введите искомый элемент: "))
 print(poisk(d, a))
 assert poisk([], 6) == None
-assert poisk([3, 7, 1, 8, 5, 9, 2, 4, 6], 0) == None
-assert poisk([3, 7, 1, 3, 1, 4, 7, 2, 8, 5, 9, 4, 6, 2, 4, 8], 8) == 13
+assert poisk([1, 2, 3, 4, 5, 6, 7, 8, 9], 0) == None
+assert poisk([1, 1, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9], 1) == 0
